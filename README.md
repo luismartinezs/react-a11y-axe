d564c80aa0d57b4be0ce12cae114f226df56574c
Luis Martinez <luismartinezwebdev@gmail.com>
2022-09-10 11:41:00 +0200

    feat: setup storybook and storybook a11y with examples

---
+++ README.md
@@ -0,0 +1,12 @@
+# React a11y
+
+Sample React project for testing a11y
+
+A11y-related dependencies:
+
+- @axe-core/react
+- @storybook/addon-a11y
+
+Some of the storybook stories are taken from this repo: https://github.com/bolonio/a11y-storybook
+
+This blogpost is worth reading to understand how the storybook a11y addon works: https://www.adrianbolonio.com/blog/accessibility-with-storybook
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +25,7 @@ function App() {
           <p>
             Edit <code>src/App.jsx</code> and save to test HMR
           </p>
+          <Button primary size="large" label="Click me" />
         </div>
         <p className="read-the-docs">
           Click on the Vite and React logos to learn more
